8871f97ca9ad5b9fc1d7774a98abe2b552d764de
Fabricio Vargas <jfabriciovr91@gmail.com>
2025-11-07 21:33:47 -0600

    chore: add CV download

--- src/lib/components/sections/AboutSection.svelte
+++ src/lib/components/sections/AboutSection.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { SectionHeadline, ExperienceTable } from '$components';
 	import photo from '$assets/about-me.webp';
+	import cv from '$assets/CV_Fabricio_Vargas.pdf';
 	import Button from '../Button.svelte';
 	import { goto } from '$app/navigation';
 	import type { WorkExperience } from '../../types/sanity';
@@ -10,6 +11,13 @@
 	}
 
 	let { experience }: AboutProps = $props();
+
+	function downloadCV() {
+		const link = document.createElement('a');
+		link.href = cv;
+		link.download = 'CV_Fabricio_Vargas.pdf';
+		link.click();
+	}
 </script>
 
 <section class="about-me mt-16">
@@ -39,8 +47,7 @@
 				your project, I’d love to hear from you. Whether you’re starting from scratch or want to
 				take your system to the next level, let’s talk and make it happen.
 			</p>
-			<Button class="mt-8" onclick={() => goto('/#contact-form')}>Tell me about your project</Button
-			>
+			<Button class="mt-8" onclick={downloadCV}>Explore my background</Button>
 		</div>
 	</div>
 	<ExperienceTable {experience} />
--- src/lib/components/sections/HeroSection.svelte
+++ src/lib/components/sections/HeroSection.svelte
@@ -1,12 +1,50 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { Button } from '$components';
+	import { onMount } from 'svelte';
+
+	let visible = false;
+
+	const text1 = "I'm a Software Developer crafting intuitive and performant";
+	const text2 = 'web experiencies';
+	const words1 = text1.split(' ');
+	const words2 = text2.split(' ');
+
+	onMount(() => {
+		visible = true;
+	});
 </script>
 
 <section class="default-margin pt-14">
 	<h3 class="text-4xl/[1.1]">Hello I'm Fabricio</h3>
 	<div class="bg-dark-gray mb-8 h-1 w-10"></div>
-	<h1>I'm a Software Developer crafting intuitive and performant</h1>
-	<h1 class="text-dark-gray mb-8">web experiencies</h1>
+	<h1>
+		{#each words1 as word, i}
+			<span class="word inline-block" class:visible style="--delay: {i * 0.1}s">
+				{word}
+			</span>{' '}
+		{/each}
+	</h1>
+	<h1 class="text-dark-gray mb-8">
+		{#each words2 as word, i}
+			<span class="word inline-block" class:visible style="--delay: {(words1.length + i) * 0.1}s">
+				{word}
+			</span>{' '}
+		{/each}
+	</h1>
 	<Button onclick={() => goto('/#contact-form')}>Let's talk</Button>
 </section>
+
+<style>
+	.word {
+		opacity: 0;
+		transform: translateX(-20px);
+		transition: all 0.6s ease-in;
+		transition-delay: var(--delay);
+	}
+
+	.word.visible {
+		opacity: 1;
+		transform: translateX(0);
+	}
+</style>
